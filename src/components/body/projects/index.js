aba80c40a94be46659c9dfea487048257c0bae86
OzerOzturk <ozerozturk1636@gmail.com>
2021-10-30 23:49:16 +0300

    project section completed using with responsive design.

--- src/components/body/projects/index.js
+++ src/components/body/projects/index.js
@@ -1,9 +1,19 @@
 import React from 'react'
-
+import Separator from '../../common/separator';
+import { ProjectData } from '../../data/projects';
+import ProjectCard from './project-card';
+import './projects.css'
 function Projects() {
+    const data=ProjectData
     return (
-        <div>
-            this is projects
+        <div className='projects'>
+            <Separator/>
+            <label className='section-title'>Featured Projects</label>
+            <div>
+                {data.map((project)=> {
+                    return <ProjectCard project={project}/>
+                })}
+            </div>
         </div>
     )
 }
